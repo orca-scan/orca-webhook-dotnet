8f9e19571cfe8dadf0bb4be5651b7674fa2fe5d4
John Doherty <contact@johndoherty.info>
2020-12-23 20:07:57 +0000

    Added very basic version of WebHookReceiver

--- Controllers/OrcaWebHookController.cs
+++ Controllers/OrcaWebHookController.cs
@@ -1,22 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace OrcaWebHookDotNet.Controllers
 {
     [ApiController]
     [Route("/")]
-    [Produces("application/json")]
     public class OrcaWebHookDotNet : ControllerBase
     {
         [HttpPost]
-        public IActionResult SaveChanges()
+        [Consumes("application/json")]
+        public async Task<OkResult> WebHookReceiver()
         {
-            // get the incoming barcode from Orca Scan
-            string barcode = HttpContext.Request.Query["barcode"].ToString();
+            // WebHooks send data as a HTTP POST, so extract JSON string from Request.Body
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {  
+                // get the raw JSON string
+                string json = await reader.ReadToEndAsync();
 
-            // go get the data from a database
+                // convert into .net object
+                dynamic data = JObject.Parse(json);
 
-            // return it as a JSON object with 200 response
-            return new OkObjectResult(new { Id = 123, barcode = barcode, Name = "Hero" });
+                // TEST: write out one of the properties
+                Console.WriteLine(data.firstName);
+            }
+
+            // always return a 200 (ok)
+            return new OkResult();
         }
     }
 }
